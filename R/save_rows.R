364492c5e95e23181ddf29beb6fb54cce8ae594c
mkinlan <morgana.kinlan@gmail.com>
2025-02-24 14:54:27 -0500

    attempting to correct score in save_rows.R again

--- R/save_rows.R
+++ R/save_rows.R
@@ -8,10 +8,11 @@
 #' @export
 #'
 #' @examples
-#' df<-data.frame(
-#' col1=c('cat','dog','bird','fish'),
+#' df<-data.frame(col1=c('cat','dog','bird','fish'),
 #' col2=c(2,3,NA,NA),
-#' col3=c('a','b','c','d'))
+#' col3=c('a','b','c','d'),
+#' stringsAsFactors = FALSE
+#' )
 #'
 #' result_df<-save_rows(df,0.33)
 
@@ -19,23 +20,24 @@ save_rows<-function(df,x) {
 
   orig_cols<-ncol(df)#create separate var to capture original len of cols
 
-  df$score=0
+  df$score_temp=0
 
   for(i in 1:nrow(df)) { #first looping by row
     for (j in 1:ncol(df)) { #then checks by col
       if (is.na(df[i,j])) {
         #print("it's null")
-        df$score[i]<- df$score[i] + 1
-        df$percent_full[i]<-1-(df$score[i]/orig_cols)
+        df$score_temp[i]<- df$score_temp[i] + 1
+        df$percent_full[i]<-1-(df$score_temp[i]/orig_cols)
       }
       else{
-        df$score[i]<- df$score[i]
-        df$percent_full[i]<-1-(df$score[i]/orig_cols)
+        df$score_temp[i]<- df$score_temp[i]
+        df$percent_full[i]<-1-(df$score_temp[i]/orig_cols)
 
       }
     }
   }
-  df<-df %>% dplyr::filter(df$percent_full >= x)
-  result<- df %>% dplyr::select(-df$score)
+  result<-df %>% dplyr::filter(df$percent_full >= x) %>% mutate(across(c('percent_full'),round,2))
+  result<-subset(result,select=-c(score_temp))
+  print(result)
   return(result)
 }
--- tests/testthat/test-save_rows.R
+++ tests/testthat/test-save_rows.R
@@ -1,5 +1,7 @@
-test_that("save_rows saves 33% of a df", {
-  expect_equal(save_rows(
+context("save_rows")
+
+testthat::test_that("save_rows saves 33% of a df", {
+  testthat::expect_equal(save_rows(
     test_df<-data.frame(col1=c('cat','dog','bird','fish'),
                         col2=c(2,3,NA,NA),
                         col3=c('a','b','c','d')),
@@ -7,7 +9,7 @@ test_that("save_rows saves 33% of a df", {
     data.frame(col1=c('cat','dog','bird','fish'),
                col2=c(2,3,NA,NA),
                col3=c('a','b','c','d'),
-               percent_full=c(1.0000000,1.0000000,0.6666667,0.6666667))
+               percent_full=c(1.00,1.00,0.67,0.67))
   )
 })
 
